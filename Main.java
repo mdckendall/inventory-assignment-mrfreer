d6dad245aa6185f26a8991551da033f8bb2724eb
Dave Freer <david.freer@gmail.com>
2022-11-16 04:52:57 +0000

    Inventory please work

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ class Inventory {
 	public String toString() {
 		return name + "," + serialNumber + "," + value;
 	}
-} //why is this???
+} //why is this??? what is happening!
 
 class Main {
 	public static void main(String[] args) {
